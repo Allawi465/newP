c41733e2a4c131135784a5ff515170c6b0dba5a0
Mohammed Allawi <91701833+Allawi465@users.noreply.github.com>
2025-09-18 17:56:10 +0200

    changing the loader

--- js/components/loader/index.js
+++ js/components/loader/index.js
@@ -2,7 +2,6 @@ import gsap from "gsap";
 import SplitType from 'split-type';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
-
 gsap.registerPlugin(ScrollTrigger);
 ScrollTrigger.normalizeScroll(true);
 
@@ -25,8 +24,6 @@ export default function initLoadingSequence(context) {
         ease: "power2.inOut",
     });
 
-    timeline.add(() => animateCounters(), 0);
-
     timeline.to("#canvas", {
         zIndex: 50
     },).to(context.largeShaderMaterial.uniforms.progress, {
@@ -37,7 +34,7 @@ export default function initLoadingSequence(context) {
         onUpdate: () => {
             const progress = context.largeShaderMaterial.uniforms.progress.value;
 
-            gsap.to(".counter", {
+            gsap.to(".panel", {
                 opacity: -progress,
                 duration: 1,
             })
@@ -88,50 +85,3 @@ export default function initLoadingSequence(context) {
         },
     }, 3);
 }
-
-export const animateCounters = () => {
-    return new Promise((resolve) => {
-        const tl = gsap.timeline({ onComplete: resolve });
-
-        for (let i = 0; i < 2; i++) {
-            for (let j = 0; j < 10; j++) {
-                const div = document.createElement('div');
-                div.className = 'num';
-                div.textContent = j;
-                document.querySelector('.counter-3').appendChild(div);
-            }
-        }
-
-        const finalDiv = document.createElement('div');
-        finalDiv.className = 'num';
-        finalDiv.textContent = '0';
-        document.querySelector('.counter-3').appendChild(finalDiv);
-
-        function animate(counter, duration, delay = 0) {
-            const numHeight = document.querySelector(counter + ' .num').clientHeight;
-            const totalDistance =
-                (document.querySelectorAll(counter + ' .num').length - 1) * numHeight;
-
-            return gsap.to(counter, {
-                duration: duration,
-                delay: delay,
-                y: -totalDistance,
-                ease: 'power2.inOut',
-            });
-        }
-
-        tl.add(animate('.counter-3', 1.5), 0)
-            .add(animate('.counter-2', 2.5), '<')
-            .add(animate('.counter-1', 1.5, 1), '<')
-            .to('.num', {
-                duration: 0.5,
-                opacity: 0,
-                delay: 1.,
-                ease: 'power2.out',
-            }, '<').to('.counter_percent', {
-                duration: 0.5,
-                opacity: 0,
-                ease: 'power2.out',
-            }, '<');
-    });
-};
