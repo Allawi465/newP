c987bdc7bb11e8a5946bae1f5c1effa8cb425bfd
Mohammed Allawi <91701833+Allawi465@users.noreply.github.com>
2025-10-11 15:17:04 +0200

    .

--- js/threeJS/scrollstrigger/lenis.js
+++ js/threeJS/scrollstrigger/lenis.js
@@ -4,21 +4,42 @@ import gsap from 'gsap';
 
 export default function setupLenis(context) {
 
-    gsap.ticker.lagSmoothing(500, 33);
-
     const lenis = new Lenis({
         wrapper: document.documentElement,
         content: document.body,
         lerp: 0.12,
-        smoothTouch: false,
+        syncTouch: false,
+        touchMultiplier: 2.0,
+        autoToggle: true,
         autoRaf: false,
+        infinite: false
     });
 
     context.bodyLenis = lenis;
 
-    lenis.on('scroll', ScrollTrigger.update);
-    gsap.ticker.add((t) => lenis.raf(t * 1000));
+    lenis.on('scroll', ScrollTrigger.update)
 
+    // drive Lenis with GSAP’s ticker
+    gsap.ticker.add((t) => lenis.raf(t * 1000));
+    gsap.ticker.lagSmoothing(0);
+    ;
+
+    // ScrollTrigger proxy
+    ScrollTrigger.scrollerProxy(document.documentElement, {
+        scrollTop(value) {
+            if (arguments.length) {
+                lenis.scrollTo(value, { immediate: true });
+            }
+            return lenis.scroll;
+        },
+        getBoundingClientRect() {
+            return { top: 0, left: 0, width: innerWidth, height: innerHeight };
+        },
+        pinType: document.documentElement.style.transform ? 'transform' : 'fixed',
+    });
+
+    lenis.scrollTo(0, { immediate: true });
+    ScrollTrigger.refresh();
 
     context.startBodyScrolling = () => lenis.start();
     context.stopBodyScrolling = () => lenis.stop();
