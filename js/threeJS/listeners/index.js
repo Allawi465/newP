2ac16a587b4749d4f15e43f8c8f5721425fdba27
Mohammed Allawi <91701833+Allawi465@users.noreply.github.com>
2025-10-13 01:00:47 +0200

    lenis

--- js/threeJS/listeners/index.js
+++ js/threeJS/listeners/index.js
@@ -1,32 +1,33 @@
 import * as THREE from 'three';
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { onPointerDown, onPointerMove, onPointerUp } from '../slider/index.js';
 import { onMouseMoveHover } from '../slider/mouseHover/index.js';
 import showAbout from '../../components/about/index.js';
 import closeInfoDiv from '../../components/close/index.js';
 import { onWindowResize } from '../index.js';
 
-gsap.registerPlugin(ScrollTrigger);
-
 export default function setupEventListeners(context) {
     window.addEventListener('resize', () => onWindowResize(context));
     window.addEventListener('mousemove', (event) => onMouseMoveHover(event, context));
     const sliderEl = document.getElementById('sliderContainer');
 
+
+    sliderEl.addEventListener('pointermove', (e) => {
+        if (!context.isDragging) return;
+        onPointerMove(e, context);
+    }, { passive: false });
+
+    sliderEl.addEventListener('touchmove', (e) => {
+        if (!context.isDragging) return;
+        onPointerMove(e, context);
+    }, { passive: false });
+
     sliderEl.addEventListener('pointerdown', (e) => onPointerDown(e, context));
-    sliderEl.addEventListener('pointermove', (e) => onPointerMove(e, context));
     sliderEl.addEventListener('pointerup', (e) => onPointerUp(e, context));
 
     sliderEl.addEventListener('touchstart', (e) => onPointerDown(e, context));
-    sliderEl.addEventListener('touchmove', (e) => onPointerMove(e, context));
     sliderEl.addEventListener('touchend', (e) => onPointerUp(e, context));
     document.getElementById('openAbout').addEventListener('click', () => showAbout(context));
     document.getElementById('close').addEventListener('click', () => closeInfoDiv(context));
-
-    window.addEventListener('load', () => {
-        ScrollTrigger.refresh();
-    });
 
     window.addEventListener('pointermove', (event) => {
         if (!context.followMouse) return;
