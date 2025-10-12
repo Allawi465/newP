76ac6a4e19cd62087ff910f68184e8e0f9c9c073
Mohammed Allawi <91701833+Allawi465@users.noreply.github.com>
2025-10-12 23:45:57 +0200

    .

--- js/threeJS/slider/planeMesh/createMeshes.js
+++ js/threeJS/slider/planeMesh/createMeshes.js
@@ -18,7 +18,7 @@ export default function createMeshes(context) {
         context.group.add(planeMesh);
     });
 
-    context.scene.add(context.group);
+    /*     context.scene.add(context.group); */
 
     context.projectsElement = document.querySelector(".projects")
 
