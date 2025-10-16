95b43ec862696ead666cb1ad5bd3e8329090131a
Mohammed Allawi <91701833+Allawi465@users.noreply.github.com>
2025-10-17 00:38:41 +0200

    debug

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+/* import tailwindcss from '@tailwindcss/vite' */
 
 export default defineConfig(({ mode }) => ({
     base: mode === 'production' ? '/newP/' : '/',
     server: { host: true },
-    plugins: [tailwindcss()],
+    /*   plugins: [tailwindcss()], */
 }))
